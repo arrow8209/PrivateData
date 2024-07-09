982973ce4e016f252cd545453439079d067c1a03
arrow8209 <arrow8209@gmail.com>
2024-07-09 21:13:55 +0800

    update cloudflare ip

--- cloudflare/bestIP.sh
+++ cloudflare/bestIP.sh
@@ -45,7 +45,7 @@ rm result_*.csv
 rm $output_file
 #process_ips result_IPDB.csv
 #process_ips result_cf.csv
-process_ips result_proxy_ip
+process_ips result_proxy_ip.csv
 
 
 # echo "结果已保存到 $output_file"
